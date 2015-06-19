67a9dffa15ddc26ec7245942cc407e4eb19e2b14
wangzhongli <hasayaqiwang@gmail.com>
2015-06-19 11:43:01 +0800

    提交

--- app/src/main/java/com/momo/lingke/MainActivity.java
+++ app/src/main/java/com/momo/lingke/MainActivity.java
@@ -1,38 +1,65 @@
 package com.momo.lingke;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.momo.lingke.view.CircleMenuLayout;
 
 
 public class MainActivity extends ActionBarActivity {
+    private CircleMenuLayout mCircleMenuLayout;
+
+    private String[] mItemTexts = new String[]{"安全中心 ", "特色服务", "投资理财",
+            "转账汇款", "我的账户", "信用卡"};
+    private int[] mItemImgs = new int[]{R.mipmap.home_mbank_1_normal,
+            R.mipmap.home_mbank_2_normal, R.mipmap.home_mbank_3_normal,
+            R.mipmap.home_mbank_4_normal, R.mipmap.home_mbank_5_normal,
+            R.mipmap.home_mbank_6_normal};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mCircleMenuLayout = (CircleMenuLayout) findViewById(R.id.id_menulayout);
+        mCircleMenuLayout.setMenuItemIconsAndTexts(mItemImgs, mItemTexts);
+        mCircleMenuLayout.setOnMenuItemClickListener(new CircleMenuLayout.OnMenuItemClickListener() {
+
+            @Override
+            public void itemClick(View view, int pos) {
+                Toast.makeText(MainActivity.this, mItemTexts[pos],
+                        Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void itemCenterClick(View view) {
+                Toast.makeText(MainActivity.this,
+                        "you can do something just like ccb  ",
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
